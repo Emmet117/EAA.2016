52a97c249ab7c1fca9251e0d89a9db7e21ea3aeb
John Burns <jzburns@gmail.com>
2016-10-13 10:32:42 +0100

    Pipe question

---
+++ Week4/pipe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#PS -EF Shows every process on the system.
+#Grep mysql: grep searches the named input files for lines containing a match to the
+#       given pattern.
+
+#ps -ef | grep mysql Shows every process that relates to mysql
